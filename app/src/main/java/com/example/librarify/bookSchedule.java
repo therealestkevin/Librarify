283ec8b951e96d46c4815ffa755954a17266d5e3
Kevin Xu <kevinxu8099@gmail.com>
2019-02-12 18:49:40 -0800

    Update Reading Schedule

--- app/src/main/java/com/example/librarify/bookSchedule.java
+++ app/src/main/java/com/example/librarify/bookSchedule.java
@@ -1,16 +1,31 @@
 package com.example.librarify;
 
 import android.os.Bundle;
-import android.widget.CalendarView;
+import android.view.View;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class bookSchedule extends AppCompatActivity {
-    private CalendarView bookSchedule;
+    private MaterialCalendarView bookSchedule;
+    private Toolbar scheduleToolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_schedule);
-        bookSchedule = findViewById(R.id.bookSchedule);
+        bookSchedule = (MaterialCalendarView) findViewById(R.id.calendarView);
+        scheduleToolBar = (Toolbar) findViewById(R.id.scheduleToolBar);
+        setSupportActionBar( scheduleToolBar);
+        scheduleToolBar.setTitle("Book Schedule");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        scheduleToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
