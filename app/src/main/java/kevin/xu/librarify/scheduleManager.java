85925a51b2831074df970b1f522e2f9b35c70aa2
Kevin Xu <kevinxu8099@gmail.com>
2019-02-16 23:50:25 -0800

    Creating Reading Events

--- app/src/main/java/kevin/xu/librarify/scheduleManager.java
+++ app/src/main/java/kevin/xu/librarify/scheduleManager.java
@@ -1,13 +1,20 @@
 package kevin.xu.librarify;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.DatePicker;
+import com.applandeo.materialcalendarview.builders.DatePickerBuilder;
+import com.applandeo.materialcalendarview.listeners.OnSelectDateListener;
 import com.xu.librarify.R;
+
+import java.util.Calendar;
+import java.util.List;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +24,12 @@ public class scheduleManager extends AppCompatActivity {
     private Toolbar scheduleManagerToolbar;
     private Button addBtnSchedule;
     private Button deleteBtnSchedule;
+    private EditText editTextTitle;
+    private EditText editTextStartPage;
+    private EditText editTextPageEnd;
+    private Button dateButton;
     private int BookPosition;
+    private AlertDialog.Builder bobBuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,22 +56,60 @@ public class scheduleManager extends AppCompatActivity {
     addBtnSchedule.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            LinearLayout layout = new LinearLayout(getApplicationContext());
-                    EditText title = new EditText(getApplicationContext());
-                    title.setHint(BookAdapter.mBook.get(BookPosition).getTitle());
-                    layout.addView(title);
-                    EditText pageNumberStart = new EditText(getApplicationContext());
 
-                    pageNumberStart.setInputType(InputType.TYPE_CLASS_NUMBER);
-                    pageNumberStart.setHint(""+1);
-                    layout.addView(pageNumberStart);
-                    EditText pageNumberEnd = new EditText(getApplicationContext());
-                    pageNumberEnd.setInputType(InputType.TYPE_CLASS_NUMBER);
-                    pageNumberEnd.setHint(BookAdapter.mBook.get(BookPosition).getBookList().getItems().get(0)
-                    .getVolumeInfo().getPageCount());
-                    layout.addView(pageNumberEnd);
-            new AlertDialog.Builder(scheduleManager.this).setView(layout)
-            .show();
+
+
+            LayoutInflater inflater = getLayoutInflater();
+
+
+
+
+
+
+
+            bobBuilder = new AlertDialog.Builder(scheduleManager.this);
+            View dialogCustomView = inflater.inflate(R.layout.custom_dialog,null);
+            bobBuilder.setView(dialogCustomView);
+            editTextTitle= dialogCustomView.findViewById(R.id.editTextTitle);
+
+            editTextTitle.setText(BookAdapter.mBook.get(BookPosition).getTitle());
+
+            editTextStartPage = dialogCustomView.findViewById(R.id.editTextStartPage);
+
+            editTextStartPage.setHint(""+BookAdapter.mBook.get(BookPosition).getBookList().getItems().get(0).getVolumeInfo().getPageCount()
+                    +" Pages In Total");
+
+            editTextPageEnd = dialogCustomView.findViewById(R.id.editTextPageEnd);
+
+            editTextPageEnd.setHint(""+BookAdapter.mBook.get(BookPosition).getBookList().getItems().get(0).getVolumeInfo().getPageCount()
+                    +" Pages In Total");
+            dateButton = dialogCustomView.findViewById(R.id.dateButton);
+            OnSelectDateListener listenerDates = new OnSelectDateListener() {
+                @Override
+                public void onSelect(List<Calendar> calendar) {
+
+                }
+            };
+            dateButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatePickerBuilder builderBob2 = new DatePickerBuilder(scheduleManager.this, listenerDates).pickerType(CalendarView.RANGE_PICKER);
+                    DatePicker realDatePicker = builderBob2.build().show();
+                }
+            });
+                 bobBuilder.setPositiveButton("Create",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    }).setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            })
+            .create().show();
 
 
 
