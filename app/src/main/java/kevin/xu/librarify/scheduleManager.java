18644c45cb99540d22c2ab807eb351dc8f149d34
Kevin Xu <30887959+therealestkevin@users.noreply.github.com>
2019-02-26 12:44:46 -0800

    MultiLine Text

--- app/src/main/java/kevin/xu/librarify/scheduleManager.java
+++ app/src/main/java/kevin/xu/librarify/scheduleManager.java
@@ -141,7 +141,7 @@ public class scheduleManager extends AppCompatActivity {
                     }).setNegativeButton("Exit", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-
+                            dialog.dismiss();
                 }
             })
             .create().show();
@@ -174,8 +174,8 @@ public class scheduleManager extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         decorView.setSystemUiVisibility(options);
